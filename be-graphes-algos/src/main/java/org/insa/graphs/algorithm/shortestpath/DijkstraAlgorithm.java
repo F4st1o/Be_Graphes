08ec7912df76ca62830bed877ec963355e2429d7
Wallart Nathan <wallart@insa-20161>
2024-05-03 10:47:11 +0200

    Avant verif auto

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,7 +1,6 @@
 package org.insa.graphs.algorithm.shortestpath;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 import org.insa.graphs.algorithm.utils.BinaryHeap;
@@ -19,42 +18,63 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        int nbNode = data.getGraph().size();
+        final int nbNode = data.getGraph().size();
         Label[] tab = new Label[nbNode];
         int cptMarque = 0;
         double coutRealise = 0;
         BinaryHeap<Label> tas = new BinaryHeap<Label>();
-
         int i;
         Label x;
+
         for(i=0;i<tab.length;i++)
         {
             tab[i] = new Label(data.getGraph().get(i), null, false);
         }
         tab[data.getOrigin().getId()].modifCoutRealise(coutRealise);
         tas.insert(tab[data.getOrigin().getId()]);
-        while(cptMarque < nbNode)
+        notifyOriginProcessed(data.getOrigin());
+        while(cptMarque < nbNode && !tas.isEmpty())
         {
             x = tas.deleteMin();
             x.setMarquetrue();
-            //si x == destinaton alors nbmarque=nbNode "boost performances"
-            cptMarque++;
-            coutRealise = x.getCoutRealise();
-            for(Arc elem : x.getCourant().getSuccessors())
+            notifyNodeMarked(x.getCourant());
+            if(x.getCourant() == data.getDestination())
             {
-                Label y = tab[elem.getDestination().getId()];
-                if(!y.getMarque())
+                cptMarque = nbNode;
+            }
+
+            else
+            {
+                cptMarque++;
+                coutRealise = x.getCoutRealise();
+                for(Arc elem : x.getCourant().getSuccessors())
                 {
-                    double coutActuel = coutRealise+elem.getMinimumTravelTime();
-                    if(coutActuel<y.getCost())
+                    if(data.isAllowed(elem))
                     {
-                        y.modifCoutRealise(coutActuel);
-                        tas.insert(y);
-                        y.modifPere(elem);
+                        Label y = tab[elem.getDestination().getId()];
+                        if(!y.getMarque())
+                        {
+                            notifyNodeReached(y.getCourant());
+                            double coutActuel = coutRealise+data.getCost(elem);
+                            if(y.getCost() == Double.POSITIVE_INFINITY)
+                            {
+                                y.modifCoutRealise(coutActuel);
+                                y.modifPere(elem);
+                                tas.insert(y);
+                            }
+
+                            else if(coutActuel < y.getCost())
+                            {
+                                tas.remove(y);
+                                y.modifCoutRealise(coutActuel);
+                                y.modifPere(elem);
+                                tas.insert(y);
+                            }
+                        }
                     }
                 }
-                
             }
+            
         }
 
         if (tab[data.getDestination().getId()].getPere() == null) 
